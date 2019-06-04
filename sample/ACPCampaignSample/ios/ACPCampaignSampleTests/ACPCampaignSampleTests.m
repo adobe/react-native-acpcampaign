0a1355efecfa001ae11b64b1c71df3aa7dc7491a
Nick Porter <nporter@adobe.com>
2019-06-04 10:27:13 -0600

    Update headers

--- sample/ACPCampaignSample/ios/ACPCampaignSampleTests/ACPCampaignSampleTests.m
+++ sample/ACPCampaignSample/ios/ACPCampaignSampleTests/ACPCampaignSampleTests.m
@@ -9,7 +9,6 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
