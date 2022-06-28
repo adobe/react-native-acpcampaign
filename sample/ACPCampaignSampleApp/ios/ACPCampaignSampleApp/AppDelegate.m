09d6b6155857e23fadc6563da758827adf41f7d5
Ryan Morales <rymorale@adobe.com>
2022-06-28 10:37:21 -0700

    add AppDelegate methods with lifecycle api calls

--- sample/ACPCampaignSampleApp/ios/ACPCampaignSampleApp/AppDelegate.m
+++ sample/ACPCampaignSampleApp/ios/ACPCampaignSampleApp/AppDelegate.m
@@ -78,6 +78,18 @@ static void InitializeFlipper(UIApplication *application) {
   return YES;
 }
 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [ACPCore lifecyclePause];
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    [ACPCore lifecycleStart:nil];
+}
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
